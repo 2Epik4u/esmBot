3d9ebfc0d4163abf729eef2be4653c9bd81b88b1
2Epik4u <justinabossmlg@gmail.com>
2024-10-06 22:37:51 -0400

    adjust image

--- commands/image-editing/stake.js
+++ commands/image-editing/stake.js
@@ -3,8 +3,9 @@ import ImageCommand from "../../classes/imageCommand.js";
 class StakeCommand extends ImageCommand {
   params = {
     water: "assets/images/stake.png",
-    gravity: 5,
-    resize: true
+    gravity: 8,
+    resize: true,
+    append: true
   };
 
   static description = "Adds the Stake watermark to an image";
