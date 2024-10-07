6d53c6d421462e3b396ce87cb0442d11adf93576
2Epik4u <justinabossmlg@gmail.com>
2024-10-06 22:19:59 -0400

    test a stake command

---
+++ commands/image-editing/stake.js
@@ -0,0 +1,16 @@
+import ImageCommand from "../../classes/imageCommand.js";
+
+class StakeCommand extends ImageCommand {
+  params = {
+    water: "assets/images/stake.png",
+    gravity: 5,
+    resize: true
+  };
+
+  static description = "Adds the Stake watermark to an image";
+
+  static noImage = "You need to provide an image/GIF to add a Stake watermark!";
+  static command = "watermark";
+}
+
+export default StakeCommand;
